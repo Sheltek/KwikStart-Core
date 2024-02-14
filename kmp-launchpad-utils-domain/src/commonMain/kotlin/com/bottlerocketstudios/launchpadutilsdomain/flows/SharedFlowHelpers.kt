0a8ce20569006e84858f4e85d7ecad8245bd2716
Eric Emery <58563293+br-Emery@users.noreply.github.com>
2024-02-14 10:56:04 -0600

    ASAA-172 - Updated SharedFlowHelpers and bumped library to 0.0.4 (#5)

--- kmp-launchpad-utils-domain/src/commonMain/kotlin/com/bottlerocketstudios/launchpadutilsdomain/flows/SharedFlowHelpers.kt
+++ kmp-launchpad-utils-domain/src/commonMain/kotlin/com/bottlerocketstudios/launchpadutilsdomain/flows/SharedFlowHelpers.kt
@@ -4,16 +4,17 @@ import com.bottlerocketstudios.launchpadutilsdomain.logger.Loggable
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 
+/**
+ *  Helper functions to get access down casted mutable SharedFlows
+ *    due to SharedFlow being covariant we must use templates with upper
+ *    bounds to show type errors at build instead of run time.
+ */
 interface SharedFlowHelpers : Loggable {
-    /**
-     *  Helper functions to get access down casted mutable SharedFlows
-     *    due to SharedFlow being covariant we must use templates with upper bounds to show type errors at build instead of run time.
-     */
-    suspend fun <T : Number?> SharedFlow<T>.emit(value: T) =
-        (this as? MutableSharedFlow<T>)?.emit(value) ?: run { log.w("BaseViewModel.emit()", "[emitValue] unable to emit value for $this") }
+    suspend fun SharedFlow<Number>.emit(value: Number) =
+        (this as? MutableSharedFlow<Number>)?.emit(value) ?: run { log.w("BaseViewModel.emit()", "[emitValue] unable to emit value for $this") }
 
-    suspend fun <T : CharSequence> SharedFlow<T>.emit(value: T) =
-        (this as? MutableSharedFlow<T>)?.emit(value) ?: run { log.w("BaseViewModel.emit()", "[emitValue] unable to emit value for $this") }
+    suspend fun SharedFlow<CharSequence>.emit(value: CharSequence) =
+        (this as? MutableSharedFlow<CharSequence>)?.emit(value) ?: run { log.w("BaseViewModel.emit()", "[emitValue] unable to emit value for $this") }
 
     suspend fun SharedFlow<Boolean>.emit(value: Boolean) =
         (this as? MutableSharedFlow<Boolean>)?.emit(value) ?: run { log.w("BaseViewModel.emit()", "[emitValue] unable to emit value for $this") }
@@ -24,15 +25,15 @@ interface SharedFlowHelpers : Loggable {
     fun <T> SharedFlow<T>.tryEmit(value: T) =
         (this as? MutableSharedFlow<T>)?.tryEmit(value) ?: run { log.w("BaseViewModel.tryEmit()", "[emitValue] unable to emit value for $this") }
 
-    fun <T : Number?> SharedFlow<T>.tryEmit(value: T) =
-        (this as? MutableSharedFlow<T>)?.tryEmit(value) ?: run { log.w("BaseViewModel.tryEmit()", "[emitValue] unable to emit value for $this") }
+    fun SharedFlow<Number>.tryEmit(value: Number) =
+        (this as? MutableSharedFlow<Number>)?.tryEmit(value) ?: run { log.w("BaseViewModel.tryEmit()", "[emitValue] unable to emit value for $this") }
 
-    fun <T : CharSequence?> SharedFlow<T>.tryEmit(value: T) =
-        (this as? MutableSharedFlow<T>)?.tryEmit(value) ?: run { log.w("BaseViewModel.tryEmit()", "[emitValue] unable to emit value for $this") }
+    fun SharedFlow<CharSequence>.tryEmit(value: CharSequence) =
+        (this as? MutableSharedFlow<CharSequence>)?.tryEmit(value) ?: run { log.w("BaseViewModel.tryEmit()", "[emitValue] unable to emit value for $this") }
 
-    fun <T : Boolean?> SharedFlow<T>.tryEmit(value: T) =
-        (this as? MutableSharedFlow<T>)?.tryEmit(value) ?: run { log.w("BaseViewModel.tryEmit()", "[emitValue] unable to emit value for $this") }
+    fun SharedFlow<Boolean>.tryEmit(value: Boolean) =
+        (this as? MutableSharedFlow<Boolean>)?.tryEmit(value) ?: run { log.w("BaseViewModel.tryEmit()", "[emitValue] unable to emit value for $this") }
 
-    fun <T : Unit?> SharedFlow<T>.tryEmit(value: T) =
-        (this as? MutableSharedFlow<T>)?.tryEmit(value) ?: run { log.w("BaseViewModel.tryEmit()", "[emitValue] unable to emit value for $this") }
+    fun SharedFlow<Unit>.tryEmit(value: Unit) =
+        (this as? MutableSharedFlow<Unit>)?.tryEmit(value) ?: run { log.w("BaseViewModel.tryEmit()", "[emitValue] unable to emit value for $this") }
 }
